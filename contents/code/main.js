4a8083e41d36de7720dbe64dfc23d983ce14269d
hk2906 <harrykelly2906@gmail.com>
2023-01-21 18:18:20 +0000

    Created numberDesktopsChanged()
    Function to detect if the user added a desktop

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,1 +1,13 @@
- 
+var desktopState = {
+    ourChange: false
+}
+
+function numberDesktopsChanged(old_number)
+{
+    // If the user has changed the number of desktops
+    if (!desktopState.ourChange) {
+
+        }
+    }
+}
+
